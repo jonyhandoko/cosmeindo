feaae43cb521564328125db77c416cf52eebf12d
jonyhandoko <asik01@gmail.com>
2015-10-15 22:52:23 +0700

    first upload

---
+++ gocart/logs/log-2015-09-22.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-09-22 16:53:15 --> 404 Page Not Found --> sbdc001
---
+++ gocart/logs/log-2015-09-25.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-09-25 15:01:42 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:42 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:42 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:42 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:42 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:42 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 15:01:52 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:14 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:14 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:15 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:15 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:16 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:16 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:45 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:45 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:46 --> Severity: Notice  --> Undefined variable: data_filter C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
+ERROR - 2015-09-25 16:54:46 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\watchinc\gocart\themes\watchinc\views\category.php 138
