a008b925530d6857425e942b73b61348b5e671a1
jonyhandoko <asik01@gmail.com>
2015-10-15 22:25:54 +0700

    first commit

---
+++ gocart/themes/watchinc/views/footer.php
@@ -0,0 +1,78 @@
+		</div>
+
+		<div class="def-footer">
+			<footer class="footer">
+				<div class="sitemap">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-2">
+								<img src="<?php echo base_url('images/watchinc/brand-small.png');?>" class="img-responsive brand">
+							</div>
+							<div class="col-sm-2">
+								<p class="lead">ABOUT WATCH INC</p>
+								<ul class="list-unstyled def-list">
+									<li><p><a href="">NEW COLLECTIONS</a></p></li>
+									<li><p><a href="">MEN'S WATCHES</a></p></li>
+									<li><p><a href="">WOMAN WATCHES</a></p></li>
+									<li><p><a href="">SALE</a></p></li>
+									<li><p><a href="">CONFIRM PAYMENT</a></p></li>
+								</ul>
+							</div>
+							<div class="col-sm-2">
+								<p class="lead">OTHER INFORMATION</p>
+								<ul class="list-unstyled def-list">
+									<li><p><a href="">RETURN & SHIPPING</a></p></li>
+									<li><p><a href="">FAQ</a></p></li>
+									<li><p><a href="">HOW TO BUY</a></p></li>
+									<li><p><a href="">WATCH INC NEWS</a></p></li>
+								</ul>
+							</div>
+							<div class="col-sm-2">
+								<p class="lead">CONTACT</p>
+								<ul class="list-unstyled">
+									<li><p class="text-icon"><img src="<?php echo base_url('images/watchinc/icon/iphone.png');?>"> (+62)85779555903</p></li>
+									<li><p class="text-icon"><img src="<?php echo base_url('images/watchinc/icon/qrcode.png');?>"> 12345</p></li>
+									<li><p class="text-icon"><img src="<?php echo base_url('images/watchinc/icon/topic.png');?>"> Watchnic</p></li>
+									<li><p class="text-icon"><img src="<?php echo base_url('images/watchinc/icon/message.png');?>"> info@watchnic.com</p></li>
+								</ul>
+							</div>
+							<div class="col-sm-4">
+								<p class="lead">NEWSLETTER</p>
+								<p>SIGN UP FOR UPDATES AND SPECIAL OFFERS</p>
+								<form class="form-inline footer-form">
+									<div class="form-group">
+										<input type="email" class="form-control">
+									</div>
+									<button type="submit" class="btn btn-black">SIGN UP</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="rights">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-6">
+								<p>Copyright 2015 Watch Inc. All Rights Reserved.</p>
+							</div>
+							<div class="col-sm-6">
+								<ul class="list-inline socials">
+									<li><a href=""><i class="fa fa-facebook"></i></a></li>
+									<li><a href=""><i class="fa fa-twitter"></i></a></li>
+									<li><a href=""><i class="fa fa-instagram"></i></a></li>
+									<li><a href=""><i class="fa fa-youtube"></i></a></li>
+									<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+		<script src="<?php echo base_url('gocart/themes/'.$this->config->item('theme').'/js/plug.js');?>"></script>
+		<script src="<?php echo base_url('gocart/themes/'.$this->config->item('theme').'/js/devs.js');?>"></script>
+		<script src="<?php echo base_url('gocart/themes/'.$this->config->item('theme').'/js/script.js');?>"></script>
+
+	</body>
+</html>
